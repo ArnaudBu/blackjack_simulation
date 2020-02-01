182feca27e783bb81af94c04e32198c612d74abd
Arnaud Buzzi <arnaud.buzzi@nomail.com>
2020-02-01 18:36:34 -0500

    Initial commit

---
+++ func.R
@@ -0,0 +1,157 @@
+###############################################################################
+#                                                                             #
+#                 Helpers for blackjack simulation                            #
+#                                                                             #
+###############################################################################
+
+
+# libraries
+
+library(R6) # object oriented programming with R
+library(data.table) # data.table for data management
+
+# utils
+
+cross <- function(l, m) sapply(l, function(x) paste(x, m))
+
+###############################################################################
+
+# Deck class
+
+Deck <- R6Class("Deck",
+                private = list(
+                  .rank = c("A", 2:10, "J", "Q", "K"),
+                  .colors = c("♠", "♥", "♦", "♣"),
+                  .cards = c()
+                ),
+                active = list(
+                  rank = function(value){
+                    if(missing(value)){
+                      private$.rank
+                    } else {
+                      stop("`$rank` is read only", call. = FALSE)
+                    }
+                  },
+                  colors = function(value){
+                    if(missing(value)){
+                      private$.colors
+                    } else {
+                      stop("`$colors` is read only", call. = FALSE)
+                    }
+                  },
+                  cards = function(value){
+                    if(missing(value)){
+                      private$.cards
+                    } else {
+                      stop("`$cards` is read only", call. = FALSE)
+                    }
+                  }
+                ),
+                public = list(
+                  initialize = function(ndecks) {
+                    private$.cards = rep(cross(private$.rank, private$.colors), ndecks)
+                  },
+                  shuffle = function(withStopToken = T) {
+                    c = sample(private$.cards[private$.cards != "Stop"])
+                    stop_pos = min(max(2,round(rnorm(1, length(c) /2, length(c) /8), 0)),length(c) - 1)
+                    if(withStopToken){
+                      private$.cards <- c(c[1:stop_pos-1], "Stop", c[stop_pos:length(c)])
+                    } else {
+                      private$.cards <- c
+                    }
+                  },
+                  draw = function(ncards = 1) {
+                    ncards <- min(ncards, length(private$.cards))
+                    if(ncards == 0){return(NA)}
+                    c <- private$.cards[1:ncards]
+                    private$.cards <- private$.cards[-(1:ncards)]
+                    return(c)
+                  }
+                )
+)
+
+# Deck initialization
+
+deck.bj <- function(ndecks = 1, withStopToken = T){
+  d <- Deck$new(ndecks)
+  d$shuffle(withStopToken)
+  return(d)
+}
+
+###############################################################################
+
+# Hand class
+
+Hand <- R6Class("Hand",
+                private = list(.cards = c()
+                ),
+                active = list(
+                  cards = function(value){
+                    if(missing(value)){
+                      private$.cards
+                    } else {
+                      stop("`$cards` is read only", call. = FALSE)
+                    }
+                  }
+                ),
+                public = list(
+                  initialize = function() {
+                    private$.cards = c()
+                  },
+                  get = function(cards) {
+                    private$.cards <- c(private$.cards, cards)
+                  },
+                  clear = function(){
+                    private$.cards <- c()
+                  },
+                  cansplit = function(){
+                    if(length(private$.cards) != 2){
+                      return(FALSE)
+                    }
+                    scores <- gsub("♠|♥|♦|♣", "", private$.cards)
+                    if (scores[1] != scores[2]){
+                      return(FALSE)
+                    }
+                    return(TRUE)
+                  },
+                  split = function(){
+                    if(length(private$.cards) != 2){
+                      stop("Unable to split")
+                    }
+                    scores <- gsub("♠|♥|♦|♣", "", private$.cards)
+                    if (scores[1] != scores[2]){
+                      stop("Unable to split")
+                    }
+                    hand1 <- hand.bj()
+                    hand2 <- hand.bj()
+                    hand1$get(private$.cards[1])
+                    hand2$get(private$.cards[2])
+                    return(list(hand1, hand2))
+                  },
+                  score = function(first_card = F, withas = T){
+                    s <- 0
+                    cards <- gsub("J|Q|K", "10", private$.cards)
+                    if(first_card){cards <- cards[1]}
+                    isas <- grepl("A", cards)
+                    for(c in cards[!isas]){
+                      s <- s + as.numeric(gsub("([0-9]+).*$", "\\1", c))
+                    }
+                    if(withas){
+                      for(c in cards[isas]){
+                        if(s <= 10){
+                          s <- s + 11
+                        } else {
+                          s <- s + 1
+                        }
+                      }
+                    }
+                    return(s)
+                  }
+                )
+)
+
+# Hand initialization
+
+hand.bj <- function(){
+  Hand$new()
+}
---
+++ run.R
@@ -0,0 +1,212 @@
+###############################################################################
+#                                                                             #
+#                 Helpers for blackjack simulation                            #
+#                                                                             #
+###############################################################################
+
+# Working environment
+
+setwd("C:/Users/abussy01/Desktop/blackjack")
+
+# Source helpers
+
+source("func.R")
+
+###############################################################################
+
+# Global variables
+
+ndecks = 8
+soft17hit = TRUE
+max_tries = 100000
+nb_hands = 3
+
+###############################################################################
+
+# Initializations
+
+dt <- data.table()
+
+# Declare new deck
+deck <- deck.bj(ndecks)
+burncard <- deck$draw() # Burn one card for a new deck
+
+# Declare dealer
+dealer <- hand.bj()
+# Declare hands
+hands <- list()
+for(i in 1:nb_hands){
+  hands[[i]] <- hand.bj()
+}
+
+# Variable to handle if the deck must be redrawn
+redeck = FALSE
+
+# Get number of cards
+nb_cards <- rep(0, 10)
+
+# Games
+game = 0
+
+###############################################################################
+
+# Utilitaries functions
+
+
+# Get card for a hand
+get_card <- function(){
+  card <- deck$draw()
+  # Set the redeck variable if the stop token is hit
+  if(card == "Stop"){
+    redeck <<- TRUE
+    card <- deck$draw()
+    # Redraw a deck if there is no more card
+  }
+  # Register the card drawn
+  score_card = as.numeric(gsub("([0-9]+).*$", "\\1",gsub("J|Q|K", "10", gsub("A", 1, card))))
+  nb_cards[score_card] <<- nb_cards[score_card] + 1
+  if(length(deck$cards) == 0){
+    deck <<- deck.bj(ndecks)
+    burncard <- deck$draw() # Burn one card for a new deck
+    nb_cards <<- rep(0, 10) # Reinitialize number of cards
+    redeck <<- F
+  }
+  return(card)
+}
+
+# Draw 2 cards for each hand
+init_hands <- function(){
+  for(hand in append(hands, dealer)){
+    hand$clear()
+    for(i in 1:2){
+      card <- get_card()
+      # Add the card to the hand
+      hand$get(card)
+    }
+  }
+}
+
+# Play for one hand
+play <- function(hand){
+  score <- hand$score()
+  
+  # Initialize matrix
+  tmp <- data.table()
+  res <- data.table(score = score,
+                    score_dealer =dealer$score(T) ,
+                    nbas = sum(grepl("A", hand$cards)),
+                    score_if_hit = NA_integer_,
+                    score_fin_dealer = NA_integer_,
+                    nb_as_out = nb_cards[1],
+                    nb_2_out = nb_cards[2],
+                    nb_3_out = nb_cards[3],
+                    nb_4_out = nb_cards[4],
+                    nb_5_out = nb_cards[5],
+                    nb_6_out = nb_cards[6],
+                    nb_7_out = nb_cards[7],
+                    nb_8_out = nb_cards[8],
+                    nb_9_out = nb_cards[9],
+                    nb_10_out = nb_cards[10])
+  
+  # Play while not bust
+  while(score < 21){
+    
+    # Get one card
+    card <- get_card()
+    
+    # Add card to hand
+    hand$get(card)
+    
+    # Compute score
+    score <- hand$score()
+    res$score_if_hit = score
+    tmp <- rbindlist(list(tmp, res))
+    # Add to matrix
+    res <- data.table(score = score,
+                      score_dealer =dealer$score(T) ,
+                      nbas = sum(grepl("A", hand$cards)),
+                      score_if_hit = NA_integer_,
+                      score_fin_dealer = NA_integer_,
+                      nb_as_out = nb_cards[1],
+                      nb_2_out = nb_cards[2],
+                      nb_3_out = nb_cards[3],
+                      nb_4_out = nb_cards[4],
+                      nb_5_out = nb_cards[5],
+                      nb_6_out = nb_cards[6],
+                      nb_7_out = nb_cards[7],
+                      nb_8_out = nb_cards[8],
+                      nb_9_out = nb_cards[9],
+                      nb_10_out = nb_cards[10])
+  }
+  if(nrow(tmp) == 0){
+    return(res)
+  } else{
+    return(tmp)
+  }
+}
+
+# Play for the dealer
+play_dealer <- function(){
+  score_dealer <- dealer$score()
+  
+  # Play while not bust
+  while(score_dealer < 17 | (score_dealer == 17 & dealer$score(withas = F) <= 10 & soft17hit)){
+    
+    # Get one card
+    card <- get_card()
+    
+    # Add card to hand
+    dealer$get(card)
+    
+    # Compute score
+    score_dealer <- dealer$score()
+  }
+  return(score_dealer)
+}
+
+###############################################################################
+
+# Run
+
+pb <- txtProgressBar(min = 0, max = max_tries, style = 3)
+
+while(nrow(dt) < max_tries){
+  init_hands()
+  g <- data.table()
+  for(hand in hands){
+    # Test splits
+    if(hand$cansplit()){
+      hsp <- hand$split()
+      for(h in hsp){
+        game <- game + 1
+        d <-play(h)
+        d$game_id <- game
+        g <- rbindlist(list(g, d), fill = T)
+      }
+    }
+    game <- game + 1
+    d <-play(hand)
+    d$game_id <- game
+    g <- rbindlist(list(g, d), fill = T)
+  }
+  g$score_fin_dealer <- play_dealer()
+  if(redeck){
+    deck <<- deck.bj(ndecks)
+    burncard <- deck$draw() # Burn one card for a new deck
+    nb_cards <<- rep(0, 10) # Reinitialize number of cards
+    redeck <<- F
+  }
+  dt <- rbindlist(list(dt, g), fill = T)
+  setTxtProgressBar(pb, nrow(dt))
+}
+
+close(pb)
+
+
+dt[, bust := ifelse(is.na(score_if_hit), FALSE, score_if_hit > 21)]
+dt[, bust_deal := score_fin_dealer > 21]
+dt[, win := score >= score_fin_dealer | bust_deal]
+dt[, win_if_hit := ifelse(is.na(score_if_hit), FALSE, score_if_hit >= score_fin_dealer & !bust)]
+dt[, should_hit := (win_if_hit | !bust) & !win]
+dt2 = dt[, .(possible_to_win = any(win | win_if_hit)), by = game_id]
+sum(dt2$possible_to_win)/ nrow(dt2)
